52c5ab790ed7379ca26edf4e0c5b8e86f09a6510
kali <kali@kali>
2026-01-06 02:02:21 +0200

    Обновление проекта: все изменения

--- server.js
+++ server.js
@@ -16,27 +16,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
 const options = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem')
 };
 
 const server = https.createServer(options, app);
-
-
-
 const io = new Server(server);
 
-// Подключение к MongoDB
-
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB подключена'))
-    .catch(err => console.error('Ошибка подключения к MongoDB:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-
-// Схема клиента
 const ClientSchema = new mongoose.Schema({
     deviceId: String,
     lastSeen: Date,
@@ -53,16 +44,15 @@ const Client = mongoose.model('Client', ClientSchema);
 app.post('/api/update', async (req, res) => {
     const { deviceId, lat, lng } = req.body;
     const update = { lastSeen: new Date(), location: { lat, lng } };
-    
+
     await Client.findOneAndUpdate({ deviceId }, update, { upsert: true });
-    
+
     io.emit('location_update', { deviceId, lat, lng });
     res.sendStatus(200);
 });
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(port, '0.0.0.0', () => console.log(`Система мониторинга запущена на порту ${port}`));
+server.listen(port, '0.0.0.0', () => console.log(`Monitoring system running on port ${port}`));
